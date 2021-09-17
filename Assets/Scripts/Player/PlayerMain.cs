694df3af263e1457068229fc5bf2e84f794d0d3a
ArshesSH <53480645+ArshesSH@users.noreply.github.com>
2021-09-17 15:54:39 +0900

    Implement Player Jump
    Implement Player Jump

--- Assets/Scripts/Player/PlayerMain.cs
+++ Assets/Scripts/Player/PlayerMain.cs
@@ -5,32 +5,73 @@ using UnityEngine;
 public class PlayerMain : MonoBehaviour
 {
     public float speed;
-    float fTurnSpeed = 30f;
+    const float fTurnSpeed = 30.0f;
+    const float fJumpPower = 10.0f;
     float fXAxis;
     float fYAxis;
     bool bIsShiftOn;
+    bool bIsSpacebarOn;
+    public bool bIsJumpNow = false;
 
     Vector3 vMove;
+    Rigidbody rigid;
     Animator animPlayer;
 
     // Start is called before the first frame update
     void Awake()
     {
+        rigid = GetComponent<Rigidbody>();
         animPlayer = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+    }
+
+    /*----------------------------------------------------------------------------------*/
+    void GetInput()
+    {
         fXAxis = Input.GetAxisRaw("Horizontal");
         fYAxis = Input.GetAxisRaw("Vertical");
         bIsShiftOn = Input.GetButton("Sprint");
+        bIsSpacebarOn = Input.GetButton("Jump");
+    }
 
+    void Move()
+    {
         vMove = new Vector3(fXAxis, 0, fYAxis).normalized;
         transform.position += vMove * speed * (bIsShiftOn ? 1.7f : 1f) * Time.deltaTime;
-        transform.LookAt(transform.position + vMove);
 
         animPlayer.SetBool("bIsMoving", vMove != Vector3.zero);
         animPlayer.SetBool("bIsSprint", bIsShiftOn);
     }
+    
+    void Turn()
+    {
+        transform.LookAt(transform.position + vMove);
+    }
+
+    void Jump()
+    {
+        if(bIsSpacebarOn && !bIsJumpNow)
+        {
+            rigid.AddForce(Vector3.up * fJumpPower, ForceMode.Impulse);
+            bIsJumpNow = true;
+        }
+    }
+
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            bIsJumpNow = false;
+        }
+    }
+    
 }
